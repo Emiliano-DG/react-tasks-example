049625a5f30e90e288cb1b24393702691cd4699a
Emiliano Geuna <emilianogeuna1994@gmail.com>
2023-02-02 14:05:41 -0300

    first commit

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useState, useEffect } from "react";
+import { tasks as data } from "../data/tasks";
+
+export const TaskContext = createContext();
+
+export function TaskContextProvider({ children }) {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  const createTask = (task) => {
+    setTasks([
+      ...tasks,
+      {
+        id: tasks.length,
+        title: task.title,
+        description: task.description,
+      },
+    ]);
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id != taskId));
+  };
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        createTask,
+        deleteTask,
+      }}
+    >
+      {children}
+    </TaskContext.Provider>
+  );
+}
